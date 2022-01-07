4902ed4ebaf3b2b52b446f1cc328e0f423a9a8a8
ArcziPT <awolk@student.agh.edu.pl>
2022-01-07 16:41:25 +0100

    add - list languages for user

--- src/main/java/xyz/wolkarkadiusz/githubservice/service/GithubUserService.java
+++ src/main/java/xyz/wolkarkadiusz/githubservice/service/GithubUserService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import xyz.wolkarkadiusz.githubservice.model.GitRepo;
 import xyz.wolkarkadiusz.githubservice.repository.GitReposRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class GithubUserService {
@@ -22,5 +24,18 @@ public class GithubUserService {
 
     public Integer getStarsCount(String username){
         return gitReposRepository.findByUsername(username).stream().reduce(0, (sum, gitRepo) -> sum + gitRepo.getStargazers_count(), Integer::sum);
+    }
+
+    public Map<String, Integer> getLanguages(String username){
+        var repoNamesStream = getRepos(username).stream().map(GitRepo::getName);
+        var languagesStream = repoNamesStream.map(repoName -> gitReposRepository.getRepoLanguages(username, repoName));
+
+        return languagesStream.reduce(new HashMap<String, Integer>(), ((sumMap, languagesMap) -> {
+            for(Map.Entry<String, Integer> entry : languagesMap.entrySet()){
+                var key = entry.getKey();
+                sumMap.put(key, sumMap.getOrDefault(key, 0) + entry.getValue());
+            }
+            return sumMap;
+        }));
     }
 }
