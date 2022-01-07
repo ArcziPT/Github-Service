4ebab904da077324e110a9369eb61d37e0229fb2
ArcziPT <awolk@student.agh.edu.pl>
2022-01-07 16:31:30 +0100

    add getRepoLanguages in GitReposRepository

--- src/main/java/xyz/wolkarkadiusz/githubservice/repository/GitReposRepository.java
+++ src/main/java/xyz/wolkarkadiusz/githubservice/repository/GitReposRepository.java
@@ -1,14 +1,13 @@
 package xyz.wolkarkadiusz.githubservice.repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 import xyz.wolkarkadiusz.githubservice.model.GitRepo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class GitReposRepository {
@@ -28,5 +27,18 @@ public class GitReposRepository {
             return new ArrayList<GitRepo>(Arrays.asList(response.getBody()));
 
         return new ArrayList<>();
+    }
+
+    public Map<String, Integer> getRepoLanguages(String username, String repo){
+        var response = restTemplate.getForEntity(baseUrl + "/repos/" + username + "/" + repo + "/languages", JsonNode.class);
+        if(response.hasBody() && response.getBody() != null){
+            var json = response.getBody();
+
+            Map<String, Integer> languages = new HashMap<>();
+            json.fields().forEachRemaining(e -> languages.put(e.getKey(), e.getValue().asInt()));
+            return languages;
+        }
+
+        return new HashMap<>();
     }
 }
